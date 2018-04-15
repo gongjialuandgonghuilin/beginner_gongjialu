0b36aed5aa8147fe771099d65803726a77a39a56
gongjualu <1368012668@qq.com>
2018-04-15 19:29:49 +0800

    change the poems

--- vector.h
+++ vector.h
@@ -1,3 +1,12 @@
 #include<class T>
 using namespace std;
-
+class <T> vector
+{
+    public:
+        friend std:ostream& operator<< (std::ostream& os,const vector<T>&v)
+        {
+            for(int i=0;i<v.m_size;++i)
+                os<<v.m_data[i]<<" ";
+            return os;
+        }
+        vector(int 
